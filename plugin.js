cab4818b8b7d57d36b4a3d13518bfc4224f0d478
albertosouza <alberto.souza.99@gmail.com>
2015-06-19 03:39:56 -0300

    add homepage route

--- plugin.js
+++ plugin.js
@@ -2,6 +2,16 @@
 module.exports = function loadPlugin(projectPath, Plugin) {
   var plugin = new Plugin(__dirname);
 
+  // set plugin routes
+  plugin.setRoutes({
+    // homepage | default home page
+    'get /': {
+      controller : 'main',
+      action     : 'index',
+      template   : 'home/index',
+      layoutName : 'fullwidth'
+    }
+  });
 
   return plugin;
 };
--- server/controllers/main.js
+++ server/controllers/main.js
@@ -8,12 +8,6 @@ module.exports = {
    */
   index: function(req, res) {
     var we = req.getWe();
-
-    res.locals.template = 'home/index';
-
-    // use fullwidth layout in home page
-    res.locals.layoutName = 'fullwidth';
-
     res.view({ title: we.config.appName });
   }
 };
