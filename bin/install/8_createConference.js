b3f7e0c0025057f0b0a0e369522d99e10461edce
albertosouza <alberto.souza.99@gmail.com>
2015-06-24 15:09:21 -0300

    update conference creation in install script

---
+++ bin/install/8_createConference.js
@@ -0,0 +1,23 @@
+var async = require('async');
+module.exports = function(we, done) {
+  we.db.models.user.find({ limit: 1}).then(function(user) {
+    we.db.models.conference.create({
+      uniquename: 'CIPSSP-V',
+      title: 'V CONGRESSO INTERNACIONAL DE PEDAGOGIA SOCIAL & SIMPÓSIO DE PÓS-GRADUAÇÃO',
+      about: 'Os Congressos Internacionais de Pedagogia Social (CIPS) são organizados pelo Grupo de Pesquisa Pedagogia Social, da Faculdade de Educação da USP, a Associação Brasileira de Pedagogia Social (ABRAPSocial) e pelas instituições de ensino superior e grupos de pesquisas nomeados neste regulamento e constituem espaços de discussão, de reflexão, de articulação e de avaliação das práticas de Educação Social, popular e comunitária que têm a Pedagogia Social como principal referencial teórico e metodológico.',
+      registrationManagerName: 'Alberto Souza',
+      registrationManagerEmail: 'contato@albertosouza.net',
+      location: 'Brasil, Rio de Janeiro',
+
+      callForPapersStartDate: Date.now(),
+      callForPapersEndDate: Date.now(),
+      registrationStartDate: Date.now(),
+      registrationEndDate: Date.now(),
+      eventStartDate: Date.now(),
+      eventEndDate: Date.now()
+    }).then(function (r) {
+      we.log.info('Conference created: ', r.id, r.title);
+      done();
+    });
+  }).catch(done);
+};
