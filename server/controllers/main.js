163b5d2933f9334bc4c6dd6bac02652d58122b55
albertosouza <alberto.souza.99@gmail.com>
2015-10-13 05:44:21 -0300

    update home page, metadata and locales

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -8,6 +8,13 @@ module.exports = {
    */
   index: function(req, res) {
     var we = req.getWe();
+
+    if (!res.locals.metadata) res.locals.metadata = {};
+
+    res.locals.metadata.description = 'Host multiple events in one installation and every event have its own content, administrative interface and compatible with We.js plugins';
+    res.locals.metadata.image = 'http://wejs.org/public/plugin/we-core/files/images/logo.png';
+    res.locals.metadata.keywords = 'Events, conferences, we.js, node.js, javascript';
+
     res.view({ title: we.config.appName });
   }
 };
