d68b790beaf9e81a3c8a4be16734ea411fe34159
Elijah Lofgren <elijahlofgren@gmail.com>
2016-07-30 09:30:07 -0500

    Minor grammar fix in events example footer
     "Build with We.js" to "Built with We.js"

--- config/site.js
+++ config/site.js
@@ -1,4 +1,4 @@
 module.exports = {
   appName: 'We.js events',
-  appFooterText: 'Build with <a href="http://wejs.org" target="_blanck">We.js</a>'
+  appFooterText: 'Built with <a href="http://wejs.org" target="_blanck">We.js</a>'
 };
